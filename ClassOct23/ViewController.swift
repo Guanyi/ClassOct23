54a67ff81bfad4b076e38466a416bb4412599edc
Guanyi Fang <a00075340@dhcp-142-232-233-145.wifi.bcit.ca>
2015-10-23 13:35:42 -0700

    read Json obj from mongolab

--- ClassOct23/ViewController.swift
+++ ClassOct23/ViewController.swift
@@ -7,12 +7,43 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        Alamofire.request(.GET, "https://api.mongolab.com/api/1/databases/firstmongo/collections/Student?apiKey=JosaVGrEGYbVAdO3q-WTOK6_mNvPOXoX")
+            .responseJSON { response in
+                print(response.request)  // original URL request
+                print(response.response) // URL response
+                print(response.data)     // server data
+                print(response.result)   // result of response serialization
+                
+                if let value = response.result.value {
+                    print("JSON: \(value)")
+                    
+                    var StudentList = JSON(value)
+                    print(StudentList.type)
+                    print(StudentList[0, "Name"])
+                    
+                    /*for student in StudentList {
+                        print(student.type)
+                    }*/
+                    var st = StudentList[0]
+                    print(st.type)
+                    print(st["Name"])
+                    
+                    /*for var i = 0; i < StudentList.count; i++ {
+                        print(StudentList[i, "Name"])
+                    }*/
+                    
+                }
+        }
     }
 
     override func didReceiveMemoryWarning() {
